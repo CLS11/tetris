91531a40cbd939643adf45bbee52380c221c6b1b
CLS11 <crystall.101600@gmail.com>
2024-09-30 02:03:36 +0530

    Contains the UI for a single grid.

---
+++ tetris/lib/pixel.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Pixel extends StatelessWidget {
+  var color;
+  var child;
+
+  Pixel({super.key, required this.color, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      margin: EdgeInsets.all(1),
+      child: Center(
+        child: Text(
+          child.toString(),
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
