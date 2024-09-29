7db9a89c6f01cf7f1a5e3cc46b21721215aaed7c
CLS11 <crystall.101600@gmail.com>
2024-09-30 02:04:16 +0530

    All types of Tetromino and directions.

---
+++ tetris/lib/value.dart
@@ -0,0 +1,20 @@
+  //Grid dimensions
+   int rowLength = 10;
+   int colLength = 20;
+
+enum Direction{
+  left,
+  right,
+  down,
+}
+
+
+enum Tetromino{
+  L,
+  J,
+  I,
+  O,
+  S,
+  Z,
+  T,
+}
