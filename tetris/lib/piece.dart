43c6145e2b56d2bf715ad9b574d533a93fce4c73
CLS11 <crystall.101600@gmail.com>
2024-09-30 12:10:16 +0530

    Position of different Tetrominos and their movements.

--- tetris/lib/piece.dart
+++ tetris/lib/piece.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:tetris/value.dart';
 
 class Piece {
@@ -8,15 +9,69 @@ class Piece {
 //The piece is just a list of numbers
   List<int> position = [];
 
-//Generate teh integers
-  void intializePiece() {
+  //Color of tetris piece
+  Color get color {
+    return tetrominoColors[type] ??
+        Colors.white; // Default to white if not found
+  }
+
+//Generate the integers
+  void initializePiece() {
     switch (type) {
       case Tetromino.L:
         position = [
-          4,
-          14,
-          24,
-          25,
+          -26,
+          -16,
+          -6,
+          -5,
+        ];
+        break;
+      case Tetromino.J:
+        position = [
+          -25,
+          -15,
+          -5,
+          -6,
+        ];
+        break;
+      case Tetromino.I:
+        position = [
+          -4,
+          -5,
+          -6,
+          -7,
+        ];
+        break;
+      case Tetromino.O:
+        position = [
+          -15,
+          -16,
+          -5,
+          -6,
+        ];
+        break;
+      case Tetromino.S:
+        position = [
+          -15,
+          -14,
+          -6,
+          -5,
+        ];
+        break;
+      case Tetromino.Z:
+        position = [
+          -17,
+          -16,
+          -6,
+          -5,
+        ];
+        break;
+      case Tetromino.T:
+        position = [
+          -26,
+          -16,
+          -6,
+          -15,
         ];
         break;
       default:
