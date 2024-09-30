e7dfbfa3ade91177716d2dc5b7593f6655290317
CLS11 <crystall.101600@gmail.com>
2024-09-30 12:11:01 +0530

    The colors for all the tetrominos.

--- tetris/lib/value.dart
+++ tetris/lib/value.dart
@@ -1,3 +1,5 @@
+import'package:flutter/material.dart';
+  
   //Grid dimensions
    int rowLength = 10;
    int colLength = 20;
@@ -18,3 +20,13 @@ enum Tetromino{
   Z,
   T,
 }
+
+const Map<Tetromino, Color> tetrominoColors = {
+  Tetromino.L: Color(0xFFFFA500), // Orange
+  Tetromino.J: Color(0xFF0000FF), // Blue
+  Tetromino.T: Color(0xFF800080), // Purple
+  Tetromino.S: Color(0xFF00FF00), // Green
+  Tetromino.Z: Color(0xFFFF0000), // Red
+  Tetromino.I: Color(0xFF00FFFF), // Cyan
+  Tetromino.O: Color(0xFFFFFF00), // Yellow
+};
